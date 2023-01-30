d226e1b9ad70c469356fb5b6356d651a097ced91
Spicytang1 <105114645+Spicytang1@users.noreply.github.com>
2023-01-30 16:16:31 +0900

    Create gcd.c
    최대공약수를 구하는 함수

---
+++ gcd.c
@@ -0,0 +1,20 @@
+int max(int n, int m)
+{
+	return (n > m) ? n : m;
+}
+
+
+int gcd(int n, int m)
+{
+	while (1)
+	{
+		m = m % n;
+		if (m == 0)
+			break;
+		n = n % m;
+		if (n == 0)
+			break;
+	}
+	return max(m, n);
+	
+}
