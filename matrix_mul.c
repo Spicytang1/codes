4a28fef679ec30c3db5929334946d1af5b68a865
Spicytang1 <105114645+Spicytang1@users.noreply.github.com>
2023-02-14 01:39:14 +0900

    Update matrix_mul.c

--- matrix_mul.c
+++ matrix_mul.c
@@ -1,18 +1,18 @@
 // 두 행렬의 곱셈을 수행하는 프로그램
 // 두 행렬 a와 b를 입력받아 res에 저장한다.
-// a는 n*m 크기이며 b는 m*k 크기이다.
+// a는 N*M 크기이며 b는 M*K 크기이다.
 
-int n, m, k;
+int N, M, K;
 
 void matmul(int** a, int** b, int** res)
 {
 
-	for (int i = 0;i < n;i++)
+	for (int i = 0;i < N;i++)
 	{
-		for (int j = 0;j < k;j++)
+		for (int j = 0;j < K;j++)
 		{
 			int sum = 0;
-			for (int l = 0;l < m;l++)
+			for (int l = 0;l < M;l++)
 			{
 				sum += a[i][l] * b[l][j];
 			}
@@ -20,7 +20,6 @@ void matmul(int** a, int** b, int** res)
 			//printf("%d ", sum);
 		}
 		//printf("\n");
-    //행렬곱의 결과를 출력함
 	}
 
 
